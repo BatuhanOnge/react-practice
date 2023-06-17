d5e05b3d8f4fb636a9c64397373c37a8d2132452
Himmet Batuhan Onge <himmet.b.onge@gmail.com>
2023-06-17 21:55:59 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import Header from "./Header";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
   const [name, setName] = useState("Guest");
+  const [posts, setPosts] = useState([]);
+
+  //Effect hooks
+
+  useEffect(function(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => setPosts(json));
+  }, []);
 
   function login(){
     setName("Batuhan");
@@ -14,6 +23,11 @@ function App() {
       <Header title={name} />
       <p>Welcome to my blog</p>
       <button onClick={login}>Log in</button>
+      <ul>
+        {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
